77e19977761a2bea13dbe26fb88d386a33b35d64
Scott McCormick <scott.t.mccormick@gmail.com>
2019-05-21 16:13:54 -0700

    Initial express ejs boilerplate.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path    = require('path')
+const app     = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(4000, () => {
+  console.log('App listening on 4000')
+})
