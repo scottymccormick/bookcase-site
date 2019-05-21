4a11a28ac3eb87b035c50802bd59e9cfc0de7439
Scott McCormick <scott.t.mccormick@gmail.com>
2019-05-21 16:20:32 -0700

    set up static files dir

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app     = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
