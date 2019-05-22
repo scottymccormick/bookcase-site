6ef1d4f5a2c40624d0d74d5605ed4def86867eab
Scott McCormick <scott.t.mccormick@gmail.com>
2019-05-21 18:30:04 -0700

    query api with default search

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path    = require('path')
+const fetch   = require('node-fetch')
 const app     = express()
 
 app.set('view engine', 'ejs')
@@ -8,7 +9,20 @@ app.set('views', path.join(__dirname, 'views'))
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  fetch('http://localhost:3001/books/search?t=Harry Potter')
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(parsedJson) {
+      // console.log(parsedJson)
+      const books = parsedJson.items.map(book => book.volumeInfo.title)
+      console.log(books)
+      res.render('index', { books })
+    })
+    .catch(function(err) {
+      console.log(err)
+      res.render('index', {books: []})
+    })
 })
 
 app.listen(4000, () => {
